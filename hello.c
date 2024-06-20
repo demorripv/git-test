21a942e4e68ae2ac80f0d317124d41fc9d7e4223
demorripv <iaideya@clemson.edu>
2024-06-19 20:36:04 -0400

    testing trigger

--- hello.c
+++ hello.c
@@ -9,7 +9,7 @@ int main()
     printf("wini is coding");
     // new brnch check
     printf("trying new branch to try the jenkins");
-    printf("testing for Dr brooks22");
+    printf("testing for Dr brooks223");
  
     return 0;
 }
